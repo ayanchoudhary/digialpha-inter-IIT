eb13ce97dcbc32efe98439fb1cf49096d5b1a790
Ayan Choudhary <ayanchoudhary1025@gmail.com>
2022-03-19 23:52:19 +0000

    remove passwd

--- server/mongo.py
+++ server/mongo.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
 # Create a new connection object and initialize with the required database
-client = MongoClient('mongodb://admin:th3_b3es7_p4ss_1_h4d@20.102.85.148:27017/')
+client = MongoClient('mongodb://admin:PASSWORD@20.102.85.148:27017/')
 db = client.da_new
