4a683830949b44029ddfd73ebd52f27d441ad8ab
Shreyaa Sharma <66686803+cypherean@users.noreply.github.com>
2022-03-26 17:26:59 +0530

    Time filter (#14)

--- client/src/edgeServer/getCompanyDetails.js
+++ client/src/edgeServer/getCompanyDetails.js
@@ -29,22 +29,24 @@ export const useGetComparisonDetails = async (companyName1, companyName2) => {
   const updateCompany1 = useStore((state) => state.updateCompany1);
   const updateCompany2 = useStore((state) => state.updateCompany2);
   const updateShowGlobalLoader = useStore((state) => state.updateShowGlobalLoader);
+  const { startDate, endDate } = useStore((state) => state.timeFilter);
 
   useEffect(() => {
     updateShowGlobalLoader(true);
   }, [companyName1, companyName2, updateShowGlobalLoader]);
 
   const response1 = useQuery(GET_COMPANY_ALL_DETAILS_BY_NAME, {
-    variables: { name: companyName1 },
+    variables: { name: companyName1, startDate, endDate },
     skip: !companyName1,
     fetchPolicy: 'cache-first',
     onCompleted: (data) => {
-      updateCompany1(data.company);
+      updateCompany1(data.company);     
+      updateShowGlobalLoader(false);
     },
   });
 
   const response2 = useQuery(GET_COMPANY_ALL_DETAILS_BY_NAME, {
-    variables: { name: companyName2 },
+    variables: { name: companyName2, startDate,endDate },
     skip: !companyName2,
     fetchPolicy: 'cache-first',
     onCompleted: (data) => {
